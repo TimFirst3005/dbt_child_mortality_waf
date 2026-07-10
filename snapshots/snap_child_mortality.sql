565af0a1bd3a847d9e77b01c70c51378e54ac5c8
TIMOTHEE OLANYI AKANJI <t.akanji@CDCI.CI>
2026-07-10 15:24:20 +0000

    snapshot snap_child_mortality (SCD Type 2, strategy check)

---
+++ snapshots/snap_child_mortality.sql
@@ -0,0 +1,20 @@
+{% snapshot snap_child_mortality %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key="country_code || '-' || year",
+        strategy='check',
+        check_cols=['under5_mortality_rate', 'mortality_severity'],
+    )
+}}
+
+select
+    country_code,
+    country_name,
+    year,
+    under5_mortality_rate,
+    mortality_severity
+from {{ ref('stg_child_mortality') }}
+
+{% endsnapshot %}
